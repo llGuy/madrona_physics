5e1c31e892c7372f5353712e3c654519eba420b9
William Wang <wyw1@stanford.edu>
2024-10-31 16:58:02 -0700

    fix cvxpy script

--- scripts/run.py
+++ scripts/run.py
@@ -1,29 +1,27 @@
+import cvxpy
 import numpy as np
 import cvxpy as cp
 import madrona_stick as s
 
-def create_symmetric_a_decomp(A):
-    evalues, evectors = np.linalg.eig(A)
-    lam_sqrt = np.diag(np.sqrt(evalues))
-    # test = evectors @ lam @ evectors.T
-    return evectors @ lam_sqrt
+def prepare_A(A):
+    # Force symmetry
+    A_cpy = np.triu(A)
+    A_cpy = A_cpy + A_cpy.T - np.diag(np.diag(A_cpy))
+    # Boost eigenvalues, tell cvxpy that A is PSD
+    A_cpy += 1e-8 * np.eye(A_cpy.shape[0])
+    A_cpy = cvxpy.psd_wrap(A_cpy)
+    return A_cpy
 
 def cvx_solve(A, v0, mu, result):
     num_contact_pts = result.shape[0] / 3
-
     if num_contact_pts == 0:
         return
 
-    A_part = create_symmetric_a_decomp(A)
-
-    A_cpy = A_part @ A_part.T
-
-    # A_cpy = cp.Parameter(A.shape, symmetric=True)
-    # A_cpy.value = A_part @ A_part.T
+    A_cpy = prepare_A(A)
 
     f = cp.Variable(result.shape[0])
 
-    selection_mat = np.zeros((int(num_contact_pts), result.shape[0]));
+    selection_mat = np.zeros((int(num_contact_pts), result.shape[0]))
     for row in range(int(num_contact_pts)):
         selection_mat[row][row * 3] = 1.0
 
@@ -38,7 +36,7 @@ def cvx_solve(A, v0, mu, result):
         # Positivity constraints on A @ f + v0
         selection_mat @ (A_cpy @ f + v0) >= 0.0
     ]
-
+    #
     for contact in range(int(num_contact_pts)):
         constraints.append(cp.SOC(cp.multiply(mu[contact], f[contact * 3]),
                                   f[contact * 3 + 1 : (contact + 1) * 3]))
