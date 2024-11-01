b15392cc147e22d7abd34b0f7affc52234a2889e
William Wang <wyw1@stanford.edu>
2024-10-31 17:53:01 -0700

    one longer stick (add new obj later), but works for now

--- src/sim.cpp
+++ src/sim.cpp
@@ -121,7 +121,7 @@ Sim::Sim(Engine &ctx,
 
     PhysicsSystem::init(ctx, cfg.rigidBodyObjMgr,
                         consts::deltaT, 1,
-                        -9.8f * math::up, 35,
+                        -9.8f * math::up, 100,
                         physicsSolverSelector,
                         cfg.cvxSolve);
     RenderingSystem::init(ctx, cfg.renderBridge);
@@ -130,21 +130,11 @@ Sim::Sim(Engine &ctx,
 
     { // Make the stick
         stick = makeDynObject(ctx,
-                              Vector3{ 0.f, 0.f, 3.01f },
-                              Quat::angleAxis(0.5f, { 0.f, 0.f, 1.f }),
-                              Diag3x3{ 1.f, 1.f, 1.f },
+                              Vector3{ 0.f, 0.f, 40.01f },
+                              Quat::angleAxis(0.5f, { 1.f, 1.f, 1.f }),
+                              Diag3x3{ 1.f, 1.f, 20.f },
                               ResponseType::Dynamic,
                               SimObject::Stick);
-        for(int i=0; i< 10; i++)
-        {
-            makeDynObject(ctx,
-                              Vector3{ 0.f, 0.f, 7.01f + (10.f * (float)i) },
-                              Quat::angleAxis(0.5f, { 0.f, 0.f, 1.f }),
-                              Diag3x3{ 1.f, 1.f, 1.f },
-                              ResponseType::Dynamic,
-                              SimObject::Stick);
-
-        }
     }
 
     { // Make the plane
