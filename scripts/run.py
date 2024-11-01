751ab2cdc26d22f80bc063d56b078034a00542dc
William Wang <wyw1@stanford.edu>
2024-11-01 13:55:17 -0700

    little bit of scipy work

--- scripts/run.py
+++ scripts/run.py
@@ -1,97 +1,134 @@
 import cvxpy
 import numpy as np
 import cvxpy as cp
-import madrona_stick as s
 from scipy.optimize import minimize
+import madrona_stick as s
 
 
-def prepare_A(A):
-    # Force symmetry
-    A_cpy = np.triu(A)
-    A_cpy = A_cpy + A_cpy.T - np.diag(np.diag(A_cpy))
-    # Boost eigenvalues, tell cvxpy that A is PSD
-    A_cpy += 1e-8 * np.eye(A_cpy.shape[0])
-    A_cpy = cvxpy.psd_wrap(A_cpy)
-    return A_cpy
-
-
-def main_obj(f, A, v0, mu, num_contact_pts, kappa):
+def main_obj(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     return 0.5 * f.T @ (A @ f) + f.T @ v0
 
 
-def constr1(f, A, v0, mu, num_contact_pts, kappa):
+def constr1(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     s = 0
     for i in range(num_contact_pts):
-        s += np.log(f[3 * i])
+        s += -kappa * np.log(f[3 * i])
+    return s
+
+
+def constr2(f, A, v0, mu, penetrations, num_contact_pts, kappa):
+    s = 0
+    for i in range(num_contact_pts):
+        s += np.log(mu[i] ** 2 * f[3 * i] ** 2 - f[3 * i + 1] ** 2 - f[3 * i + 2] ** 2)
     return -kappa * s
 
 
-def constr2(f, A, v0, mu, num_contact_pts, kappa):
+def constr3(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     s = 0
+    vc = A @ f + v0
     for i in range(num_contact_pts):
-        s += np.log(mu[3 * i] ** 2 * f[3 * i] ** 2 - f[3 * i + 1] ** 2 - f[3 * i + 2] ** 2)
+        s += np.log(vc[3 * i] - penetrations[i])
     return -kappa * s
 
 
-def constr3(f, A, v0, mu, num_contact_pts, kappa):
-    s = 0
-    for i in range(num_contact_pts):
-        s += np.log((A @ f + v0)[3 * i])
-    return -kappa * s
-
-
-def d_main_obj(f, A, v0, mu, num_contact_pts, kappa):
+def d_main_obj(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     return A @ f + v0
 
 
-def d_constr1(f, A, v0, mu, num_contact_pts, kappa):
+def d_constr1(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     d = np.zeros(f.shape[0])
     for i in range(num_contact_pts):
-        d[3 * i] = -kappa * 1 / f[3 * i]
+        d[3 * i] = -kappa / f[3 * i]
     return d
 
 
-def d_constr2(f, A, v0, mu, num_contact_pts, kappa):
+def d_constr2(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     d = np.zeros(f.shape[0])
     for i in range(num_contact_pts):
-        s = mu[3 * i] ** 2 * f[3 * i] ** 2 - f[3 * i + 1] ** 2 - f[3 * i + 2] ** 2
-        d[3 * i] = -kappa * 2 * mu[3 * i] ** 2 * f[3 * i] / s
-        d[3 * i + 1] = kappa * 2 * f[3 * i + 1] / s
-        d[3 * i + 2] = kappa * 2 * f[3 * i + 2] / s
+        s = mu[i] ** 2 * f[3 * i] ** 2 - f[3 * i + 1] ** 2 - f[3 * i + 2] ** 2
+        d[3 * i] = -2 * kappa * mu[i] ** 2 * f[3 * i] / s
+        d[3 * i + 1] = 2 * kappa * f[3 * i + 1] / s
+        d[3 * i + 2] = 2 * kappa * f[3 * i + 2] / s
     return d
 
 
-def d_constr3(f, A, v0, mu, num_contact_pts, kappa):
+def d_constr3(f, A, v0, mu, penetrations, num_contact_pts, kappa):
     d = np.zeros(f.shape[0])
     vc = A @ f + v0
     for i in range(num_contact_pts):
-        s = vc[3 * i]
+        s = vc[3 * i] - penetrations[i]
         d += -kappa * A[3 * i, :] / s
     return d
 
 
-def scipy_solve(A, v0, mu, result):
-    num_contact_pts = result.shape[0] / 3
+def H_main_obj(f, A, v0, mu, penetrations, num_contact_pts, kappa):
+    return A
+
+
+def H_constr1(f, A, v0, mu, penetrations, num_contact_pts, kappa):
+    H = np.zeros((f.shape[0], f.shape[0]))
+    for i in range(num_contact_pts):
+        H[3 * i, 3 * i] = kappa / (f[3 * i] ** 2)
+    return H
+
+
+def H_constr2(f, A, v0, mu, penetrations, num_contact_pts, kappa):
+    H = np.zeros((f.shape[0], f.shape[0]))
+    for i in range(num_contact_pts):
+        s = mu[i] ** 2 * f[3 * i] ** 2 - f[3 * i + 1] ** 2 - f[3 * i + 2] ** 2
+        H[3 * i, 3 * i] = -2 * kappa * mu[i] ** 2 / s
+        H[3 * i + 1, 3 * i + 1] = 2 * kappa / s
+        H[3 * i + 2, 3 * i + 2] = 2 * kappa / s
+    return H
+
+
+def H_constr3(f, A, v0, mu, penetrations, num_contact_pts, kappa):
+    H = np.zeros((f.shape[0], f.shape[0]))
+    vc = A @ f + v0
+    for i in range(num_contact_pts):
+        s = vc[3 * i] - penetrations[i]
+        H += kappa * np.outer(A[3 * i, :], A[3 * i, :]) / (s ** 2)
+    return H
+
+
+def scipy_solve(A, v0, mu, penetrations, result):
+    num_contact_pts = result.shape[0] // 3
     if num_contact_pts == 0:
         return
 
-    kappa= 1e-6
-    f0 = np.zeros(result.shape[0])
-    for i in range(int(num_contact_pts)):
-        f0[3 * i] = 1000
-    res = minimize(main_obj, x0=f0,
-                   args=(A, v0, mu, num_contact_pts, kappa),
-                   jac=d_main_obj,
-                   method='BFGS')
-    print(res.x)
+    kappa = 1e-9
+
+    def total_obj(f):
+        return main_obj(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            constr1(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            constr2(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            constr3(f, A, v0, mu, penetrations, num_contact_pts, kappa)
+
+    def total_d_obj(f):
+        return d_main_obj(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            d_constr1(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            d_constr2(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            d_constr3(f, A, v0, mu, penetrations, num_contact_pts, kappa)
+
+    def total_H_obj(f):
+        return H_main_obj(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            H_constr1(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            H_constr2(f, A, v0, mu, penetrations, num_contact_pts, kappa) + \
+            H_constr3(f, A, v0, mu, penetrations, num_contact_pts, kappa)
+
+    clarabel_result = cvx_solve(A, v0, mu, penetrations, np.zeros(result.shape[0]))
+    f0 = clarabel_result
+    res = minimize(total_obj, f0, jac=total_d_obj, hess=total_H_obj, method='trust-ncg')
     result[:] = res.x
+
 
 def cvx_solve(A, v0, mu, penetrations, result):
     num_contact_pts = result.shape[0] / 3
     if num_contact_pts == 0:
         return
 
-    A_cpy = prepare_A(A)
+    A_cpy = cvxpy.psd_wrap(A)
+    R = 1e-8 * np.eye(result.shape[0])
 
     f = cp.Variable(result.shape[0])
 
@@ -99,8 +136,8 @@ def cvx_solve(A, v0, mu, penetrations, result):
     for row in range(int(num_contact_pts)):
         selection_mat[row][row * 3] = 1.0
 
-    # Objective function: 0.5 * f.T @ A @ f + f.T @ v0
-    objective = 0.5 * cp.quad_form(f, A_cpy) + f.T @ v0
+    # Objective function: 0.5 * f.T @ (A + R) @ f + f.T @ v0
+    objective = 0.5 * cp.quad_form(f, A_cpy + R) + f.T @ v0
 
     # Constraints
     constraints = [
@@ -121,6 +158,8 @@ def cvx_solve(A, v0, mu, penetrations, result):
     problem.solve()
 
     result[:] = f.value
+
+    return f.value
 
 
 if __name__ == "__main__":
