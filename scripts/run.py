incorrect signs somewhere, but contacts working :)

scripts/run.py
@@ -49,9 +49,6 @@ def create_clarabel_matrices(n_contact_pts, mu, P, c, penetration):
     return A, b, cones
 
 def clarabel_solve(A, v0, num_contact_pts):
-    if num_contact_pts == 0:
-        return
-
     # TODO: fix me
     penetrations = np.zeros(int(num_contact_pts))
     mu = 0.5 * np.ones(int(num_contact_pts))
@@ -121,13 +118,13 @@ def mass_solve(M, bias, v, J, h, result):
 
     M_inv = np.linalg.inv(M)
     A = J @ M_inv @ J.T
-    v0 = J @ (v + h * M_inv @ bias)
+    v0 = -J @ (v + h * M_inv @ bias)
 
     if num_contacts_pts == 0:
         result[:] = M_inv @ -bias
     else:
         f = clarabel_solve(A, v0, num_contacts_pts)
-        contact_imp = (J.T @ f) / h
+        contact_imp = -(J.T @ f) / h
         result[:] = M_inv @ (-bias + contact_imp)
     # temp = result[3]
     # result[3] = result[4]