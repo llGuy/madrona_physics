ec484eefdd95b783e74ab0402aded2fb70101e80
William Wang <wyw1@stanford.edu>
2024-11-19 18:39:39 -0800

    Start of cone solver

--- scripts/run.py
+++ scripts/run.py
@@ -2,6 +2,7 @@ import clarabel
 import cvxpy
 import numpy as np
 import cvxpy as cp
+import scipy
 import scipy.sparse as sp
 import madrona_stick as s
 
@@ -109,23 +110,88 @@ def cvx_solve(A, v0, mu, penetrations, result):
 
     return f.value
 
-def mass_solve(M, bias, v, J, mu, penetrations, h, result):
-    import scipy
+
+def cone_solve(M, bias, v, J, mu, penetrations, h, result):
+    """
+    Solves the objective function: (note that Af + v0 is v_c)
+        min (1/2) * f^T A f + f^T v0 + q(Af + v0)
+        subject to f \in K
+    q is an imposed penalty function to prevent penetration
+    """
     num_contacts_pts = int(J.shape[0] / 3)
-
     C = -bias
-
     M_inv = np.linalg.inv(M)
+
+    # Sparse matrices
     M_sc = scipy.sparse.csc_matrix(M)
-    A = J @ M_inv @ J.T
-    v0 = J @ (v + h * M_inv @ C)
+    J_sc = scipy.sparse.csc_matrix(J)
+    A = J_sc @ M_inv @ J_sc.T
+    v0 = J_sc @ (v + h * M_inv @ C)
+
+    def q(v_c):
+        # penalize where normal component of v_C is negative
+        ind_neg = np.where(v_c < 0)
+        ind_normal = np.arange(1, len(v_c), 3)
+        ind_neg_normal = np.intersect1d(ind_neg, ind_normal)
+        # squared loss
+        return np.sum(v_c[ind_neg_normal] ** 2)
+
+    def dq(v_c):
+        out = np.zeros_like(v_c)
+        # penalize where normal component of v_C is negative
+        ind_neg = np.where(v_c < 0)
+        ind_normal = np.arange(1, len(v_c), 3)
+        ind_neg_normal = np.intersect1d(ind_neg, ind_normal)
+        # gradient of squared loss
+        out[ind_neg_normal] = 2 * v_c[ind_neg_normal]
+        return out
+
+    def hq(v_c):
+        out = np.zeros((len(v_c), len(v_c)))
+        # penalize where normal component of v_C is negative
+        ind_neg = np.where(v_c < 0)
+        ind_normal = np.arange(1, len(v_c), 3)
+        ind_neg_normal = np.intersect1d(ind_neg, ind_normal)
+        # hessian of squared loss
+        out[ind_neg_normal, ind_neg_normal] = 2
+        return out
+
+    def obj(f):
+        return 0.5 * f.T @ A @ f + f.T @ v0 + q(A @ f + v0)
+
+    def d_obj(f):
+        return A @ f + v0 + dq(A @ f + v0)
+
+    def h_obj(f):
+        return A + hq(A @ f + v0)
 
     if num_contacts_pts == 0:
         gen_forces = C
-        result[:] = M_inv @ C
+    else:
+        f = scipy.optimize.minimize(obj, x0=np.zeros(num_contacts_pts * 3), jac=d_obj, hess=h_obj, method='trust-ncg')
+        contact_imp = (J_sc.T @ f.x) / h
+        gen_forces = C + contact_imp
+
+    res = scipy.sparse.linalg.spsolve(M_sc, gen_forces)
+    result[:] = res
+    return
+
+def mass_solve(M, bias, v, J, mu, penetrations, h, result):
+    num_contacts_pts = int(J.shape[0] / 3)
+    C = -bias
+    M_inv = np.linalg.inv(M)
+
+    # Sparse matrices
+    M_sc = scipy.sparse.csc_matrix(M)
+    J_sc = scipy.sparse.csc_matrix(J)
+    A = J_sc @ M_inv @ J_sc.T
+    v0 = J_sc @ (v + h * M_inv @ C)
+
+    if num_contacts_pts == 0:
+        gen_forces = C
     else:
         f = clarabel_solve(A, v0, mu, penetrations, num_contacts_pts)
-        contact_imp = (J.T @ f) / h
+        contact_imp = (J_sc.T @ f) / h
         gen_forces = C + contact_imp
     res = scipy.sparse.linalg.spsolve(M_sc, gen_forces)
     result[:] = res
