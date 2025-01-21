995eae1e7adab60ecfbab8f35e532dd36239e383
William Wang <wyw1@stanford.edu>
2025-01-21 01:19:34 -0800

    Update python tolerances to match

--- scripts/conjugate_gradient.py
+++ scripts/conjugate_gradient.py
@@ -10,8 +10,9 @@ from line_search import exact_line_search
 def nonlinear_cg(f, df, x0, tol, ls_tol, M, a_free, J, a_ref, mus, precision):
     max_iter = len(x0)
     # Scale the tolerance by the diagonal sum of the mass matrix
-    total_m = M.diag_sum()
-    scale = 1 / total_m
+    # total_m = M.diag_sum()
+    # scale = 1 / total_m
+    scale = np.sqrt(1 / x0.size)
 
     # Initialize
     fun = f(x0)
@@ -26,7 +27,8 @@ def nonlinear_cg(f, df, x0, tol, ls_tol, M, a_free, J, a_ref, mus, precision):
             break
 
         # Exact line search
-        line_search_tol = tol * ls_tol * np.linalg.norm(p) / scale
+        # line_search_tol = tol * ls_tol * np.linalg.norm(p) / scale
+        line_search_tol = ls_tol
         alpha = exact_line_search(x, p, line_search_tol, M, a_free, J, a_ref,
                                   mus, precision)
         if alpha == 0:
@@ -43,7 +45,7 @@ def nonlinear_cg(f, df, x0, tol, ls_tol, M, a_free, J, a_ref, mus, precision):
         if scale * (fun - fun_new) < tol:
             break
 
-        beta = np.dot(g_new, (Mgrad_new - M_grad)) / max(np.dot(g, M_grad), 1e-15)
+        beta = np.dot(g_new, (Mgrad_new - M_grad)) / max(np.dot(g, M_grad), 1e-12)
         beta = max(0, beta)
         p_new = -Mgrad_new + beta * p
         x, g, p, M_grad, fun = x_new, g_new, p_new, Mgrad_new, fun_new
